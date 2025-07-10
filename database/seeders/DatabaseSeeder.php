04b57c59a1b6e0c3f8745f59f3a2aa668b85082d
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-07-10 10:26:05 -0700

    Adds location data to the seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -53,6 +53,135 @@ class DatabaseSeeder extends Seeder
         ]);
         $farmer->assignRole($farmerRole);
 
-        Farm::factory(10)->create();
+        $farms = Farm::factory(10)->create();
+
+        // Spokane area sample locations
+        $spokaneLocations = [
+            [
+                'address' => '123 Green Bluff Rd',
+                'city' => 'Spokane',
+                'state' => 'WA',
+                'zip' => '99208',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.7897,
+                'longitude' => -117.3506,
+                'label' => 'Green Bluff Orchard',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '456 Valley Chapel Rd',
+                'city' => 'Spokane Valley',
+                'state' => 'WA',
+                'zip' => '99037',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.6562,
+                'longitude' => -117.2340,
+                'label' => 'Valley Chapel Farm',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '789 Wellesley Ave',
+                'city' => 'Spokane',
+                'state' => 'WA',
+                'zip' => '99205',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.7032,
+                'longitude' => -117.4398,
+                'label' => 'Wellesley Urban Farm',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '101 Liberty Lake Rd',
+                'city' => 'Liberty Lake',
+                'state' => 'WA',
+                'zip' => '99019',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.6769,
+                'longitude' => -117.0856,
+                'label' => 'Liberty Lake Fields',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '202 Cheney-Spokane Rd',
+                'city' => 'Cheney',
+                'state' => 'WA',
+                'zip' => '99004',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.4871,
+                'longitude' => -117.5758,
+                'label' => 'Cheney Prairie Farm',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '303 Airway Heights Blvd',
+                'city' => 'Airway Heights',
+                'state' => 'WA',
+                'zip' => '99001',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.6444,
+                'longitude' => -117.5865,
+                'label' => 'Airway Heights Acres',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '404 South Hill Dr',
+                'city' => 'Spokane',
+                'state' => 'WA',
+                'zip' => '99223',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.6149,
+                'longitude' => -117.3717,
+                'label' => 'South Hill Gardens',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '505 Five Mile Rd',
+                'city' => 'Spokane',
+                'state' => 'WA',
+                'zip' => '99208',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.7401,
+                'longitude' => -117.4392,
+                'label' => 'Five Mile Prairie Farm',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '606 Dishman Mica Rd',
+                'city' => 'Spokane Valley',
+                'state' => 'WA',
+                'zip' => '99206',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.6276,
+                'longitude' => -117.2606,
+                'label' => 'Dishman Mica Farm',
+                'timezone' => 'America/Los_Angeles',
+            ],
+            [
+                'address' => '707 Indian Trail Rd',
+                'city' => 'Spokane',
+                'state' => 'WA',
+                'zip' => '99208',
+                'county' => 'Spokane',
+                'country' => 'USA',
+                'latitude' => 47.7291,
+                'longitude' => -117.4877,
+                'label' => 'Indian Trail Ranch',
+                'timezone' => 'America/Los_Angeles',
+            ],
+        ];
+
+        foreach ($farms as $i => $farm) {
+            $locationData = $spokaneLocations[$i % count($spokaneLocations)];
+            $farm->location()->create($locationData);
+        }
     }
 }
